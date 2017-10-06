d74a8758f69f7b569d62e83ea43884bcfc3074b9
Moriah <moriahkwashington@gmail.com>
2017-10-06 16:04:03 -0400

    This is the first commit.

---
+++ LabNumber9/Circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber9
+{
+    class Circle
+    {
+        private double radius;
+
+        public double GetCircumference()
+        {
+
+            double circumference = 2 * Math.PI * radius;
+            
+            return circumference;
+        }
+
+
+        public double GetArea()
+        {   
+            double area = Math.PI * (radius * radius);
+            
+            return area;
+        }
+
+
+        
+
+        public double Radius
+        {
+            set { radius = value; }
+
+            get { return radius; }
+
+        }
+    }
+}
---
+++ LabNumber9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the radius of the circle.");
+            double radius = ReadValidInteger();
+                
+
+            Circle CircleInfo = new Circle(); //creating the object of the class
+
+            CircleInfo.Radius = radius; // assigning user value to class property
+
+            Console.WriteLine(CircleInfo.GetCircumference());
+
+            Console.WriteLine(CircleInfo.GetArea());
+
+        }
+
+        //method for validation of user input
+        public static double ReadValidInteger()
+        {
+
+            double Input = 0;
+
+            while (! double.TryParse(Console.ReadLine(), out Input)) //converts string to a number as long as the number is valid. returns true or false.
+            {
+                Console.WriteLine("Please enter a number!");
+            }
+            return Input;
+
+        }
+
+    }
+}
